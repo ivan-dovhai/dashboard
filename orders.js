5d96f6ca2961d6f1c8d267b7ca8fc0f98cc0f5d1
Ivan Dovhai <troopermanowar@gmail.com>
2023-09-09 14:21:44 +0300

    Start

---
+++ orders.js
@@ -0,0 +1,20 @@
+const orders = [
+    {
+        productName: 'PHP Tuturial',
+        productNumber: 46585,
+        paymentStatus: 'Due',
+        starus: 'Pending'
+    },
+    {
+        productName: 'JavaScript Tuturial',
+        productNumber: 626978,
+        paymentStatus: 'Refunded',
+        starus: 'Declined'
+    },
+    {
+        productName: 'CSS Tuturial',
+        productNumber: 491698,
+        paymentStatus: 'Paid',
+        starus: 'Active'
+    },
+];
